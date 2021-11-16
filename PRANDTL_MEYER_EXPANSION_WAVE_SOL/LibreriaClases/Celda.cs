f298fa23a4a212bfd6168b98f9c44e80be1fd45e
Olga <olga.ca.ar@gmail.com>
2021-11-16 23:21:56 +0100

    He empezado a ponerlo en WPF

---
+++ PRANDTL_MEYER_EXPANSION_WAVE_SOL/LibreriaClases/Celda.cs
@@ -0,0 +1,105 @@
+﻿using System;
+
+namespace LibreriaClases
+{
+    public class Celda
+    {
+        double M;
+        double u;
+        double v;
+        double T;
+        double P;
+        double Rho;
+        double a;
+        double M_angle;
+
+        double F1;
+        double F2;
+        double F3;
+        double F4;
+
+        double G1;
+        double G2;
+        double G3;
+        double G4;
+
+        double F1_p;
+        double F2_p;
+        double F3_p;
+        double F4_p;
+
+
+        double G1_p;
+        double G2_p;
+        double G3_p;
+        double G4_p;
+
+        double dF1_x;
+        double dF2_x;
+        double dF3_x;
+        double dF4_x;
+
+        double dF1_p_x;
+        double dF2_p_x;
+        double dF3_p_x;
+        double dF4_p_x;
+
+        double x;
+        double y;
+        double y_t;
+        double y_s;
+        double h;
+        double m1;
+        double m2;
+        double delta_y_t;
+
+        double P_p;
+
+
+        public void Transformation(double H, double E, double Theta)
+        { if (x < E)
+            {
+                y_s = 0;
+                h = H;
+                m1 = 0;
+            }
+            else
+            {
+                y_s = -x * Math.Tan(Theta) + E * Math.Tan(Theta);
+                h = H + x * Math.Tan(Theta) - E * Math.Tan(Theta);
+                m1 = (Math.Tan(Theta) / h) - y_t * (Math.Tan(Theta) / h);
+            }
+            y = y_t * h + y_s;
+            m2 = 1 / h;       
+        }
+
+        public double StepSize(double Theta, double delta_y, double M_angle)
+        {
+            double tan1 = Math.Abs(Math.Tan(Theta*(Math.PI/180) + M_angle));
+            double tan2 = Math.Abs(Math.Tan(Theta * (Math.PI / 180) - M_angle));
+
+            double tan_max = Math.Max(tan1, tan2);
+            double delta_x = 0.5 * delta_y / tan_max;
+
+            return delta_x;
+        }
+
+
+        public double[] PredictorStep(double delta_y_t, double F1_arriba, double F2_arriba,double F3_arriba, double F4_arriba, double G1_arriba,double G2_arriba, double G3_arriba, double G4_arriba)
+        { 
+            dF1_x = (m1 * (F1 - F1_arriba) / delta_y_t) + (m2 * (G1 - G1_arriba) / delta_y_t);
+            dF2_x = (m1 * (F2 - F2_arriba) / delta_y_t) + (m2 * (G2 - G2_arriba) / delta_y_t);
+            dF3_x = (m1 * (F3 - F3_arriba) / delta_y_t) + (m2 * (G3 - G3_arriba) / delta_y_t);
+            dF3_x = (m1 * (F4 - F4_arriba) / delta_y_t) + (m2 * (G4 - G4_arriba) / delta_y_t);
+
+           
+            double[] vector = new double[] { };
+
+            return vector ;
+        }
+
+
+       
+
+    }
+}
---
+++ PRANDTL_MEYER_EXPANSION_WAVE_SOL/LibreriaClases/Normas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibreriaClases
+{
+    class Normas
+    {
+        double H = 40;
+        double Theta = 5.352*Math.PI/180; 
+        double L = 65; 
+        double E = 10; 
+
+        double M_in = 2; 
+        double P_in = 101325; 
+        double Rho_in = 1.225; 
+        double R_air = 287; 
+        double Gamma = 1.4;
+        double T_in=288.2031;
+        double a_in=340.2940; 
+        double v_in = 0; 
+        double u_in=680.588; 
+        double M_angle=0.5236;
+
+    }
+}
