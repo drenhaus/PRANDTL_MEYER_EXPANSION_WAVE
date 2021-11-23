249ab7bf2119c6f364c19401ef23fb7413ac4410
Olga <olga.ca.ar@gmail.com>
2021-11-23 21:41:57 +0100

    divisiones eta y xi cambiadas

--- PRANDTL_MEYER_EXPANSION_WAVE_SOL/LibreriaClases/Malla.cs
+++ PRANDTL_MEYER_EXPANSION_WAVE_SOL/LibreriaClases/Malla.cs
@@ -7,8 +7,8 @@ namespace LibreriaClases
 {
     class Malla
     {
-        int divisiones_xi=41; // nº filas -- i
-        int divisiones_eta=81; // nº columnas -- j
+        int divisiones_eta=41; // nº filas -- i
+        int divisiones_xi = 81; // nº columnas -- j
         double delta_y_t = 0.025;
         double Cy = 0.5;
         Normas norma = new Normas();
@@ -27,15 +27,15 @@ namespace LibreriaClases
         public void DefinirMatriz(int filas, int columnas)
         {
 
-            this.divisiones_xi = columnas;
             this.divisiones_eta = filas;
+            this.divisiones_xi = columnas;
 
             this.matriz = new Celda[filas, columnas];
 
             //We fill the matrix with Cells
-            for (int i = 0; i < divisiones_xi; i++)
+            for (int i = 0; i < divisiones_eta; i++)
             {
-                for (int j = 0; j < divisiones_eta; j++)
+                for (int j = 0; j < divisiones_xi; j++)
                 {
                     matriz[i, j] = new Celda();
                 }
@@ -50,7 +50,7 @@ namespace LibreriaClases
             norma.Compute_u();
 
             // We define the initial line conditions
-            for (int i = 0; i < divisiones_xi; i++)
+            for (int i = 0; i < divisiones_eta; i++)
             {
 
                 matriz[i, 0].a = norma.a_in;
@@ -64,20 +64,20 @@ namespace LibreriaClases
                 matriz[i, 0].Compute_G_F(norma.Gamma);
 
             }
-            for (int j = 0; j < divisiones_eta; j++)
+            for (int j = 0; j < divisiones_xi; j++)
             {
-                for (int i = 1; i < divisiones_xi; i++)
+                for (int i = 1; i < divisiones_eta; i++)
                 {
                     matriz[i, j].Compute_y_t(matriz[i - 1, j].y_t, delta_y_t);
                 }
             }
 
 
-            for (int j = 0; j < divisiones_eta; j++)
+            for (int j = 0; j < divisiones_xi; j++)
             {
                 if (matriz[0,j].x<=norma.L)
                 {
-                    for (int i = 0; i < divisiones_xi; i++)
+                    for (int i = 0; i < divisiones_eta; i++)
                     {
                         matriz[i, j].xy_Transformation_ToEtaXi(norma.H, norma.E, norma.Theta);
                         double[] TanMax_deltax = new double[2];
@@ -89,11 +89,11 @@ namespace LibreriaClases
                         {
                             F1_F2_F3_F4_p_derecha_vector = matriz[i, j].Predictor_Step_Contorno_Inferior(delta_y_t, TanMax_deltax[2], matriz[i + 1, j].F1, matriz[i + 1, j].F2, matriz[i + 1, j].F3, matriz[i + 1, j].F4, matriz[i + 1, j].G1, matriz[i + 1, j].G2, matriz[i + 1, j].G3, matriz[i + 1, j].G4);
                         }
-                        if (i == divisiones_xi-1)
+                        if (i == divisiones_eta - 1)
                         {
                             F1_F2_F3_F4_p_derecha_vector = matriz[i, j].Predictor_Step_Contorno_Superior(delta_y_t, TanMax_deltax[2], matriz[i - 1, j].F1, matriz[i - 1, j].F2, matriz[i - 1, j].F3, matriz[i - 1, j].F4, matriz[i - 1, j].G1, matriz[i - 1, j].G2, matriz[i - 1, j].G3, matriz[i - 1, j].G4);
                         }
-                        if (i > 0 && i < divisiones_xi)
+                        if (i > 0 && i < divisiones_eta)
                         {
                             F1_F2_F3_F4_p_derecha_vector = matriz[i, j].Predictor_Step_Principal(Cy, delta_y_t, TanMax_deltax[2], matriz[i + 1, j].F1, matriz[i + 1, j].F2, matriz[i + 1, j].F3, matriz[i + 1, j].F4, matriz[i - 1, j].F1, matriz[i - 1, j].F2, matriz[i - 1, j].F3, matriz[i - 1, j].F4, matriz[i + 1, j].G1, matriz[i + 1, j].G2, matriz[i + 1, j].G3, matriz[i + 1, j].G4, matriz[i + 1, j].P, matriz[i - 1, j].P);
                         }
