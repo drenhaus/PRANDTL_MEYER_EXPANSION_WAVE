df409a512bb2763a12d04f91b91fe0136a1bee66
Drenhaus <marc.frederic.drenhaus@estudiantat.upc.edu>
2021-11-28 14:11:04 +0100

    from tables donete

--- PRANDTL_MEYER_EXPANSION_WAVE_SOL/WPFapp/WelcomeWindow.xaml.cs
+++ PRANDTL_MEYER_EXPANSION_WAVE_SOL/WPFapp/WelcomeWindow.xaml.cs
@@ -23,8 +23,8 @@ namespace WPFapp
         }
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            label_explicacion.Visibility = Visibility.Visible;
-            label_explicacion.Content = "The objective of this project is to simulate and obtain a numerical solution of a flow over a Prandtl-Meyer expansion corner. In this simulation, we are going to suppose that the flow that moves on the surface is two-dimensional, supersonic and invisible and we are going to establish a series of initial conditions in order to simplify the problem and obtain the main properties of the flow that interests us to study its behaviour.";
+            //label_explicacion.Visibility = Visibility.Visible;
+            //label_explicacion.Content = "The objective of this project is to simulate and obtain a numerical solution of a flow over a Prandtl-Meyer expansion corner. In this simulation, we are going to suppose that the flow that moves on the surface is two-dimensional, supersonic and invisible and we are going to establish a series of initial conditions in order to simplify the problem and obtain the main properties of the flow that interests us to study its behaviour.";
             //Load text for main label explaining the project
         }
 
