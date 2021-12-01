a6d5aa817770aa20205842b3773c82d3293978a2
Drenhaus <marc.frederic.drenhaus@estudiantat.upc.edu>
2021-12-01 23:01:19 +0100

    vamos vamos

---
+++ PRANDTL_MEYER_EXPANSION_WAVE_SOL/LibreriaClases/Punto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibreriaClases
+{
+    public class Punto
+    {
+        // Definimos una clase punto con un valor x e y
+        public double X { get; set; }
+        public double Y { get; set; }
+        public Punto(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+        public override string ToString()
+        {
+            return string.Format("({0},{1})", X, Y);
+        }
+    }
+}
