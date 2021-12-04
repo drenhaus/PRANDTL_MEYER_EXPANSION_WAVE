92a3ec9657a725ed5bad50c6364695f628c7529f
Olga <olga.ca.ar@gmail.com>
2021-12-04 16:53:22 +0100

    ya exporta bien los datos

--- PRANDTL_MEYER_EXPANSION_WAVE_SOL/LibreriaClases/ExportExcel.cs
+++ PRANDTL_MEYER_EXPANSION_WAVE_SOL/LibreriaClases/ExportExcel.cs
@@ -11,37 +11,33 @@ namespace LibreriaClases
 {
     public static class ExportExcel
     {
-        public static void ExportToExcel(this DataTable tbl, string excelFilePath = null)
+        public static void ExportToExcel(this DataTable tbl)
         {
             try
             {
-                // load excel, and create a new workbook
-                var excelApp = new Excel.Application();
-                excelApp.Workbooks.Add();
+                var new_excel = new Excel.Application();
+                new_excel.Workbooks.Add();
 
-                // single worksheet
-                Excel._Worksheet workSheet = excelApp.ActiveSheet;
+                Excel._Worksheet workSheet = new_excel.ActiveSheet;
 
-                // column headings
-                for (var i = 0; i < tbl.Columns.Count; i++)
+                for (int i = 0; i < tbl.Columns.Count; i++)
                 {
                     workSheet.Cells[1, i + 1] = tbl.Columns[i].ColumnName;
                 }
 
-                // rows
-                for (var i = 0; i < tbl.Rows.Count; i++)
+                for (int i = 0; i < tbl.Rows.Count; i++)
                 {
-                    // to do: format datetime values before printing
-                    for (var j = 0; j < tbl.Columns.Count; j++)
+
+                    for (int j = 0; j < tbl.Columns.Count; j++)
                     {
-                        workSheet.Cells[i + 2, j + 1] = tbl.Rows[i][j];
+                        workSheet.Cells[i + 2, j + 1] = Convert.ToDouble(tbl.Rows[i][j]);
                     }
                 }
-                    excelApp.Visible = true;
+                new_excel.Visible = true;
             }
             catch (Exception ex)
             {
-                throw new Exception("ExportToExcel: \n" + ex.Message);
+                throw new Exception(ex.Message);
             }
         }
 
