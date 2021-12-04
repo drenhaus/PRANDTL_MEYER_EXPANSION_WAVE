7770f2e8e5c6a33a94fd708bab6f8c5bc321ae54
Drenhaus <marc.frederic.drenhaus@estudiantat.upc.edu>
2021-12-04 19:33:42 +0100

    segue rundinant la tia

--- PRANDTL_MEYER_EXPANSION_WAVE_SOL/WPFapp/MainWindow.xaml.cs
+++ PRANDTL_MEYER_EXPANSION_WAVE_SOL/WPFapp/MainWindow.xaml.cs
@@ -317,24 +317,23 @@ namespace WPFapp
             AdvancedStudyWindow ad_w = new AdvancedStudyWindow();
             ad_w.Show();
 
+            // m2 sera la primera malla del caso 1 (caso 1 el de dos pendientes de angulo distinto juntas)
+            double sumaTheta = (10 * (Math.PI) / 180);
+
+
             Malla m2 = new Malla(); // Parimos de cero, es decir, nueva malla y reacemos el calculo de una matriz en resolucion media
             //la idea es hacer otra matriz con los datos recogidos de la ultima columna de la simulacion.
 
-            columnas = 89;
-            filas = 41;
-            delta_y_t = 0.025;
 
-            double sumaTheta = (10 * (Math.PI) / 180);
-
-            m2.rows = filas;
-            m2.columns = columnas;
-            m2.delta_y_t = delta_y_t;
+            m2.rows = 41;
+            m2.columns = 89;
+            m2.delta_y_t = 0.025;
 
             m2.norma.L = 55;
             m2.norma.E = 10;
             m2.norma.H = 40;
 
-            m2.norma.Theta = sumaTheta *(1 / 3);
+            m2.norma.Theta = sumaTheta/ 3;
 
             m2.norma.v_in = 0;
             m2.norma.Rho_in = 1.23;
@@ -349,7 +348,6 @@ namespace WPFapp
 
             m2.DefinirMatriz();
             m2.Compute();
-            //m2.Fill_DataTable();
 
             List<Celda> ListadeUltimaColumnadeCeldas = GetLastColumOfMatriz(m2);
 
@@ -357,18 +355,19 @@ namespace WPFapp
             
             Malla m3 = new Malla();
 
-            columnas = 92;
-            filas = 41;
-            delta_y_t = 0.025;
+            m3.rows = 41;
+            m3.columns = 92;
+            m3.delta_y_t = 0.025;
 
-            m3.rows = filas;
-            m3.columns = columnas;
-            m3.delta_y_t = delta_y_t;
+            double incrementoDeXenPrimerPendiente = m2.norma.L - m2.norma.E;
+            double difrenciadeAltura = incrementoDeXenPrimerPendiente * Math.Tan(m2.norma.Theta);
 
-            m3.rows = filas;
-            m3.columns = columnas;
-            m3.delta_y_t = delta_y_t;
 
+
+
+            m3.norma.L = 45;
+            m3.norma.E = 1;
+            m3.norma.H = 40+ difrenciadeAltura;
 
             m3.DefinirMatriz();
             m3.Compute2(ListadeUltimaColumnadeCeldas);
