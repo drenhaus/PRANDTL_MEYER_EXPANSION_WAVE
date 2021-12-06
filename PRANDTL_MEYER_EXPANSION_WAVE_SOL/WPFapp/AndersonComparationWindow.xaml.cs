208e6bd72ce7691bf3066d24dc79b1ce67a5afd8
Olga <olga.ca.ar@gmail.com>
2021-12-06 21:13:15 +0100

    ya funciona

--- PRANDTL_MEYER_EXPANSION_WAVE_SOL/WPFapp/AndersonComparationWindow.xaml.cs
+++ PRANDTL_MEYER_EXPANSION_WAVE_SOL/WPFapp/AndersonComparationWindow.xaml.cs
@@ -22,6 +22,10 @@ namespace WPFapp
         public AndersonComparationWindow()
         {
             InitializeComponent();
+            anderson_error.Visibility = Visibility.Hidden;
+            anderson_label.Visibility = Visibility.Hidden;
+            computed_label.Visibility = Visibility.Hidden;
+            our_error.Visibility = Visibility.Hidden;
         }
 
         double Anderson_M_error;
@@ -670,19 +674,19 @@ namespace WPFapp
 
         public void Compute_Errors()
         {
-            Anderson_M_error = 100 * Math.Abs(0.45 - MeanValue(Anderson_M_66)) / 0.45;    
-            Anderson_p_error = 100 * Math.Abs(0.739*Math.Pow(10,5) - MeanValue(Anderson_p_66)) / (0.739 * Math.Pow(10, 5));
-            Anderson_rho_error = 100 * Math.Abs(0.984 - MeanValue(Anderson_rho_66)) / 0.984;
-            Anderson_t_error = 100 * Math.Abs(262 - MeanValue(Anderson_T_66)) / 262;
-            Anderson_u_error = 100 * Math.Abs(710 - MeanValue(Anderson_u_66)) / 710;
-            Anderson_v_error = 100 * Math.Abs(-66.5 - MeanValue(Anderson_v_66)) / -66.5;
+            Anderson_M_error = Math.Round(100 * Math.Abs(2.2 - MeanValue(Anderson_M_66)) / 2.2,4);    
+            Anderson_p_error = Math.Round(100 * Math.Abs(0.739*Math.Pow(10,5) - MeanValue(Anderson_p_66)) / (0.739 * Math.Pow(10, 5)),4);
+            Anderson_rho_error = Math.Round(100 * Math.Abs(0.984 - MeanValue(Anderson_rho_66)) / 0.984,4);
+            Anderson_t_error = Math.Round(100 * Math.Abs(262 - MeanValue(Anderson_T_66)) / 262,4);
+            Anderson_u_error = Math.Round(100 * Math.Abs(710 - MeanValue(Anderson_u_66)) / 710,4);
+            Anderson_v_error =Math.Abs( Math.Round(100 * Math.Abs(-66.5 - MeanValue(Anderson_v_66)) / -66.5,4));
             
-            our_M_error= 100 * Math.Abs(0.45 - MeanValue(our_M_66)) / 0.45;
-            our_p_error= 100 * Math.Abs(0.739 * Math.Pow(10, 5) - MeanValue(our_p_66)) / (0.739 * Math.Pow(10, 5));
-            our_rho_error= 100 * Math.Abs(0.984 - MeanValue(our_rho_66)) / 0.984;
-            our_t_error= 100 * Math.Abs(262 - MeanValue(our_T_66)) / 262;
-            our_u_error= 100 * Math.Abs(710 - MeanValue(our_u_66)) / 710;
-            our_v_error= 100 * Math.Abs(-66.5 - MeanValue(our_v_66)) / -66.5;
+            our_M_error= Math.Round(100 * Math.Abs(2.2 - MeanValue(our_M_66)) / 2.2,4);
+            our_p_error= Math.Round(100 * Math.Abs(0.739 * Math.Pow(10, 5) - MeanValue(our_p_66)) / (0.739 * Math.Pow(10, 5)),4);
+            our_rho_error= Math.Round(100 * Math.Abs(0.984 - MeanValue(our_rho_66)) / 0.984,4);
+            our_t_error= Math.Round(100 * Math.Abs(262 - MeanValue(our_T_66)) / 262,4);
+            our_u_error= Math.Round(100 * Math.Abs(710 - MeanValue(our_u_66)) / 710,4);
+            our_v_error= Math.Abs(Math.Round(100 * Math.Abs(-66.5 - MeanValue(our_v_66)) / -66.5,4));
 
         }
 
@@ -708,91 +712,209 @@ namespace WPFapp
             if (Setect_Parameter_ComboBox.SelectedIndex == 0 && Setect_X_ComboBox.SelectedIndex==0) //T AND 12
             { AndersonGridData.DataContext = Anderson_T_12.DefaultView;
               SimulationGrid.DataContext = our_T_12.DefaultView;
+
+                anderson_error.Visibility= Visibility.Hidden;
+                anderson_label.Visibility= Visibility.Hidden;
+                computed_label.Visibility= Visibility.Hidden;
+                our_error.Visibility= Visibility.Hidden;
+
             }
             if (Setect_Parameter_ComboBox.SelectedIndex == 0 && Setect_X_ComboBox.SelectedIndex == 1) //T AND 66
             {AndersonGridData.DataContext = Anderson_T_66.DefaultView;
              SimulationGrid.DataContext = our_T_66.DefaultView;
-                anderson_error.Text = "0.038";
+
+                anderson_error.Visibility = Visibility.Visible;
+                anderson_label.Visibility = Visibility.Visible;
+                computed_label.Visibility = Visibility.Visible;
+                our_error.Visibility = Visibility.Visible;
+
+                anderson_error.Text = Convert.ToString(Anderson_t_error);
+                our_error.Text = Convert.ToString(our_t_error);
             }
             if (Setect_Parameter_ComboBox.SelectedIndex == 1 && Setect_X_ComboBox.SelectedIndex == 0) //u AND 12
             { AndersonGridData.DataContext = Anderson_u_12.DefaultView;
               SimulationGrid.DataContext = our_u_12.DefaultView;
+
+                anderson_error.Visibility = Visibility.Hidden;
+                anderson_label.Visibility = Visibility.Hidden;
+                computed_label.Visibility = Visibility.Hidden;
+                our_error.Visibility = Visibility.Hidden;
             }
             if (Setect_Parameter_ComboBox.SelectedIndex == 1 && Setect_X_ComboBox.SelectedIndex == 1) //u AND 66
             { AndersonGridData.DataContext = Anderson_u_66.DefaultView;
               SimulationGrid.DataContext = our_u_66.DefaultView;
+
+                anderson_error.Visibility = Visibility.Visible;
+                anderson_label.Visibility = Visibility.Visible;
+                computed_label.Visibility = Visibility.Visible;
+                our_error.Visibility = Visibility.Visible;
+
+                anderson_error.Text = Convert.ToString(Anderson_u_error);
+                our_error.Text = Convert.ToString(our_u_error);
             }
 
             if (Setect_Parameter_ComboBox.SelectedIndex == 2 && Setect_X_ComboBox.SelectedIndex == 0) //v AND 12
             { AndersonGridData.DataContext = Anderson_v_12.DefaultView;
               SimulationGrid.DataContext = our_v_12.DefaultView;
+
+                anderson_error.Visibility = Visibility.Hidden;
+                anderson_label.Visibility = Visibility.Hidden;
+                computed_label.Visibility = Visibility.Hidden;
+                our_error.Visibility = Visibility.Hidden;
             }
             if (Setect_Parameter_ComboBox.SelectedIndex == 2 && Setect_X_ComboBox.SelectedIndex == 1) //v AND 66
             { AndersonGridData.DataContext = Anderson_v_66.DefaultView;
               SimulationGrid.DataContext = our_v_12.DefaultView;
+
+                anderson_error.Visibility = Visibility.Visible;
+                anderson_label.Visibility = Visibility.Visible;
+                computed_label.Visibility = Visibility.Visible;
+                our_error.Visibility = Visibility.Visible;
+
+                anderson_error.Text = Convert.ToString(Anderson_v_error);
+                our_error.Text = Convert.ToString(our_v_error);
             }
 
             if (Setect_Parameter_ComboBox.SelectedIndex == 3 && Setect_X_ComboBox.SelectedIndex == 0) //rho AND 12
             { AndersonGridData.DataContext = Anderson_rho_12.DefaultView;
               SimulationGrid.DataContext = our_rho_12.DefaultView;
+
+                anderson_error.Visibility = Visibility.Hidden;
+                anderson_label.Visibility = Visibility.Hidden;
+                computed_label.Visibility = Visibility.Hidden;
+                our_error.Visibility = Visibility.Hidden;
             }
             if (Setect_Parameter_ComboBox.SelectedIndex == 3 && Setect_X_ComboBox.SelectedIndex == 1) //rho AND 66
             { AndersonGridData.DataContext = Anderson_rho_66.DefaultView;
               SimulationGrid.DataContext = our_rho_12.DefaultView;
+
+                anderson_error.Visibility = Visibility.Visible;
+                anderson_label.Visibility = Visibility.Visible;
+                computed_label.Visibility = Visibility.Visible;
+                our_error.Visibility = Visibility.Visible;
+
+                anderson_error.Text = Convert.ToString(Anderson_rho_error);
+                our_error.Text = Convert.ToString(our_rho_error);
             }
 
             if (Setect_Parameter_ComboBox.SelectedIndex == 4 && Setect_X_ComboBox.SelectedIndex == 0) //p AND 12
             { AndersonGridData.DataContext = Anderson_p_12.DefaultView;
               SimulationGrid.DataContext = our_p_12.DefaultView;
+
+                anderson_error.Visibility = Visibility.Hidden;
+                anderson_label.Visibility = Visibility.Hidden;
+                computed_label.Visibility = Visibility.Hidden;
+                our_error.Visibility = Visibility.Hidden;
             }
             if (Setect_Parameter_ComboBox.SelectedIndex == 4 && Setect_X_ComboBox.SelectedIndex == 1) //p AND 66
             { AndersonGridData.DataContext = Anderson_p_66.DefaultView;
               SimulationGrid.DataContext = our_p_66.DefaultView;
+
+                anderson_error.Visibility = Visibility.Visible;
+                anderson_label.Visibility = Visibility.Visible;
+                computed_label.Visibility = Visibility.Visible;
+                our_error.Visibility = Visibility.Visible;
+
+                anderson_error.Text = Convert.ToString(Anderson_p_error);
+                our_error.Text = Convert.ToString(our_p_error);
             }
 
             if (Setect_Parameter_ComboBox.SelectedIndex == 5 && Setect_X_ComboBox.SelectedIndex == 0) //M AND 12
             { AndersonGridData.DataContext = Anderson_M_12.DefaultView;
               SimulationGrid.DataContext = our_M_12.DefaultView;
+
+                anderson_error.Visibility = Visibility.Hidden;
+                anderson_label.Visibility = Visibility.Hidden;
+                computed_label.Visibility = Visibility.Hidden;
+                our_error.Visibility = Visibility.Hidden;
             }
             if (Setect_Parameter_ComboBox.SelectedIndex == 5 && Setect_X_ComboBox.SelectedIndex == 1) //M AND 66
             { AndersonGridData.DataContext = Anderson_M_66.DefaultView;
               SimulationGrid.DataContext = our_M_66.DefaultView;
+
+                anderson_error.Visibility = Visibility.Visible;
+                anderson_label.Visibility = Visibility.Visible;
+                computed_label.Visibility = Visibility.Visible;
+                our_error.Visibility = Visibility.Visible;
+
+                anderson_error.Text = Convert.ToString(Anderson_M_error);
+                our_error.Text = Convert.ToString(our_M_error);
             }
 
             if (Setect_Parameter_ComboBox.SelectedIndex == 6 && Setect_X_ComboBox.SelectedIndex == 0) //F1 AND 12
             { AndersonGridData.DataContext = Anderson_F1_12.DefaultView;
               SimulationGrid.DataContext = our_F1_12.DefaultView;
+
+                anderson_error.Visibility = Visibility.Hidden;
+                anderson_label.Visibility = Visibility.Hidden;
+                computed_label.Visibility = Visibility.Hidden;
+                our_error.Visibility = Visibility.Hidden;
             }
             if (Setect_Parameter_ComboBox.SelectedIndex == 6 && Setect_X_ComboBox.SelectedIndex == 1) //F1 AND 66
             { AndersonGridData.DataContext = Anderson_F1_66.DefaultView;
               SimulationGrid.DataContext = our_F1_66.DefaultView;
+
+                anderson_error.Visibility = Visibility.Hidden;
+                anderson_label.Visibility = Visibility.Hidden;
+                computed_label.Visibility = Visibility.Hidden;
+                our_error.Visibility = Visibility.Hidden;
             }
 
             if (Setect_Parameter_ComboBox.SelectedIndex == 7 && Setect_X_ComboBox.SelectedIndex == 0) //F2 AND 12
             { AndersonGridData.DataContext = Anderson_F2_12.DefaultView;
               SimulationGrid.DataContext = our_F2_12.DefaultView;
+
+                anderson_error.Visibility = Visibility.Hidden;
+                anderson_label.Visibility = Visibility.Hidden;
+                computed_label.Visibility = Visibility.Hidden;
+                our_error.Visibility = Visibility.Hidden;
             }
             if (Setect_Parameter_ComboBox.SelectedIndex == 7 && Setect_X_ComboBox.SelectedIndex == 1) //F2 AND 66
             { AndersonGridData.DataContext = Anderson_F2_66.DefaultView;
               SimulationGrid.DataContext = our_F2_66.DefaultView;
+
+                anderson_error.Visibility = Visibility.Hidden;
+                anderson_label.Visibility = Visibility.Hidden;
+                computed_label.Visibility = Visibility.Hidden;
+                our_error.Visibility = Visibility.Hidden;
             }
 
             if (Setect_Parameter_ComboBox.SelectedIndex == 8 && Setect_X_ComboBox.SelectedIndex == 0) //F3 AND 12
             { AndersonGridData.DataContext = Anderson_F3_12.DefaultView;
               SimulationGrid.DataContext = our_F3_12.DefaultView;
+
+                anderson_error.Visibility = Visibility.Hidden;
+                anderson_label.Visibility = Visibility.Hidden;
+                computed_label.Visibility = Visibility.Hidden;
+                our_error.Visibility = Visibility.Hidden;
             }
             if (Setect_Parameter_ComboBox.SelectedIndex == 8 && Setect_X_ComboBox.SelectedIndex == 1) //F3 AND 66
             { AndersonGridData.DataContext = Anderson_F3_66.DefaultView;
               SimulationGrid.DataContext = our_F3_66.DefaultView;
+
+                anderson_error.Visibility = Visibility.Hidden;
+                anderson_label.Visibility = Visibility.Hidden;
+                computed_label.Visibility = Visibility.Hidden;
+                our_error.Visibility = Visibility.Hidden;
             }
 
             if (Setect_Parameter_ComboBox.SelectedIndex == 9 && Setect_X_ComboBox.SelectedIndex == 0) //F4 AND 12
             { AndersonGridData.DataContext = Anderson_F4_12.DefaultView;
               SimulationGrid.DataContext = our_F4_12.DefaultView;
+
+                anderson_error.Visibility = Visibility.Hidden;
+                anderson_label.Visibility = Visibility.Hidden;
+                computed_label.Visibility = Visibility.Hidden;
+                our_error.Visibility = Visibility.Hidden;
             }
             if (Setect_Parameter_ComboBox.SelectedIndex == 9 && Setect_X_ComboBox.SelectedIndex == 1) //F4 AND 66
             { AndersonGridData.DataContext = Anderson_F4_66.DefaultView;
               SimulationGrid.DataContext = our_F4_66.DefaultView;
+
+                anderson_error.Visibility = Visibility.Hidden;
+                anderson_label.Visibility = Visibility.Hidden;
+                computed_label.Visibility = Visibility.Hidden;
+                our_error.Visibility = Visibility.Hidden;
             }
         }
     }
